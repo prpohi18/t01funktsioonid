862658fd13e40a23a96c99930c00269b7d4f1f78
Sten Markus Laht <lahtsten@greeny.cs.tlu.ee>
2018-01-29 11:45:25 +0200

    esimene kodutöö

---
+++ andmed2.java
@@ -0,0 +1,22 @@
+public class andmed2{
+	public static int leiaSumma(int[] arvud){
+		int summa=0;
+		for(int arv: arvud){summa+=arv;}
+		return summa;
+	}
+		
+	public static int[] arvudeks(String[] andmed){
+		int[] arvud=new int[andmed.length];
+		for(int i=0; i<andmed.length; i++){
+			arvud[i]=Integer.parseInt(andmed[i]);
+		}
+		return arvud;
+	}
+
+	public static void main(String[] arg){
+		int[] a=arvudeks(arg);
+		System.out.println(leiaSumma(a));
+		
+	}
+}
+
