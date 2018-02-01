3cb20f9969e4f2d53a60cf41f50fe751aa4ab9d9
if17 <if17@A406-1.INFORMAATIKA>
2018-02-01 11:30:56 +0200

    Pindala arvutamise funktsioon

---
+++ Kodutöö1/src/kodutöö1/Kodutöö1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kodutöö1;
+
+/**
+ *
+ * @author if17
+ */
+public class Kodutöö1 {
+    static double pindala(double a, double b){
+        return a*b;
+    }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(pindala(6, 4));
+    }
+    
+}
