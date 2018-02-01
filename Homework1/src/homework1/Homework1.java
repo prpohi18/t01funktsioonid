1802566c0dceca0f2ecc4b35b33479f14f83d0c0
Kaur <kaur@mail.ee>
2018-02-01 20:40:25 +0200

    Homework. Added test version (pindala)

---
+++ Homework1/src/homework1/Homework1.java
@@ -0,0 +1,29 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework1;
+
+/**
+ *
+ * @author Kaur
+ */
+public class Homework1 {
+    
+    static double pindala(double a, double b){
+        return a*b;
+    }
+            
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Pindala on: "+pindala(+3,4)+"cm.");
+    }
+    
+}
