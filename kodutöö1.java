98a27557c00e4e60f5d82b463560b2638f533183
Sten Markus Laht <lahtsten@greeny.cs.tlu.ee>
2018-02-04 23:45:11 +0200

    Esimene

--- kodutöö1.java
+++ kodutöö1.java
@@ -1,32 +1,24 @@
-/*public class kodutöö1{
-	public static void main(String[] arg){
-		int vanus=20;
-		if (vanus>17){
-			System.out.println("Täis");
-		}
-		else System.out.println("Alakas");
-	}
-}
-		
-*/
 import java.util.Scanner;
 public class kodutöö1{
 	public static void main(String[] args){
 		String name;
 		int arv1, arv2, arv3;
-		double average;
+		double keskmine;
+		double summa;
 		
 		Scanner input= new Scanner (System.in);
 		
-		System.out.println ("Sisesta kolm arvu ja leitakse nende keskmine");
+		System.out.println ("Sisesta kolm arvu ja leitakse nende summa ning keskmine");
 		arv1= input.nextInt();
 		arv2= input.nextInt();
 		arv3= input.nextInt();
 		
 		input.close();
 		
-		average= (double) (arv1+arv2+arv3)/3;
-		System.out.println("Arvude keskmine on" + average);
+		keskmine= (double) (arv1+arv2+arv3)/3;
+		summa= (double) (arv1+arv2+arv3);
+		System.out.println("Arvude summa on " + summa);
+		System.out.println("Arvude keskmine on " + keskmine);
 	}
 }
 		
