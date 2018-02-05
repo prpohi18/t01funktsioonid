5fad7b3ca56af32be40cc8df1a7822c0a717b682
Robert Roos <31948767+roosen7@users.noreply.github.com>
2018-02-05 09:55:26 +0200

    Create NKylgnetaringrun.java

---
+++ NKylgnetaringrun.java
@@ -0,0 +1,9 @@
+public class NKylgneTaringRun{
+		
+	public static void main(String[] args) {
+		//koodi loogika
+		Taring d12 = new Taring(12); //teeb 12-kyljelise taringu
+		Taring tavaline = new Taring(6); //tavaline täring
+		System.out.println(tavaline.veereta()); //siit saan vahetada täringut	
+	}	
+}
