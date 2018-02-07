39f7269e6e08c142d8668f9926f8c658a0c60ee0
Mihkel Jäe <mjae@tlu.ee>
2018-02-08 00:44:55 +0200

    Kodutöö, Mihkel Jäe

---
+++ kodutoo/src/kodutoo/Kodutoo.java
@@ -0,0 +1,40 @@
+package Kodutoo;
+
+import java.util.Scanner;
+
+public class Kodutoo {
+    
+    public static void main(String[] args) {
+        
+        double height = 0.0;
+        double weight = 0.0;
+        double bmi = 0.0;
+        
+        Scanner s = new Scanner(System.in);
+        System.out.println("funktsioon aitab arvutada kehamassiineksit.");
+        
+        System.out.print("Pikkus(cm): ");
+        height = s.nextInt();
+        System.out.print("Kaal(kg): ");
+        weight = s.nextInt();
+        
+        bmi = ((weight)/(height/100 * height/100));
+        
+        
+        System.out.printf("Sinu kehamassiindeks on %f\n", bmi);
+        
+        System.out.println("Kehamassiindeksi vahemikud");
+        System.out.println("Alakaal < 18.9");
+        System.out.println("Normaalkaal 19 - 25 ");
+        System.out.println("Ülekaal < 25.1");   
+    }
+}
+
+/*funktsioon aitab arvutada kehamassiineksit.
+Pikkus(cm): 187
+Kaal(kg): 76
+Sinu kehamassiindeks on 21.733535
+Kehamassiindeksi vahemikud
+Alakaal < 18.9
+Normaalkaal 19 - 25 
+Ülekaal < 25.1*/
