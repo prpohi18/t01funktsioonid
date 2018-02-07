f0fcc044ee64056e39616e2aaf7a8f7d04d09c5a
Kaur <kaur@mail.ee>
2018-02-07 23:15:44 +0200

    Fine tuning

--- Homework1/src/homework1/Homework1.java
+++ Homework1/src/homework1/Homework1.java
@@ -14,7 +14,7 @@ package homework1;
 public class Homework1 {
    //Programm BTC hinnamuutuse arvutamiseks
     public static void btcHind (double uusHind){
-        double vanaHind = 13657.2020;
+        double vanaHind = 13657.20;
         if(uusHind>vanaHind){System.out.println("Bitcoini hind on aasta algusest t6usnud "+((uusHind*100/vanaHind)-100)+"% ehk "+(uusHind-vanaHind)+" eurot.");}
         else if(uusHind<vanaHind){System.out.println("Bitcoini hind on aasta algusest langenud "+(100-(uusHind*100/vanaHind))+"% ehk "+(vanaHind-uusHind)+" eurot.");}
         else {System.out.println("Bitcoini hind on j22nud aasta algusega samaks ehk "+vanaHind+" eurot.");}
@@ -25,7 +25,7 @@ public class Homework1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Sisestan praeguse BTC hinna, et saada teada muutust
+        //Sisestan praeguse BTC hinna, et saada teada muutust ning prindin selle v2lja
         System.out.println("Arvutame BTC hinnamuutuse alates 01.01.2018.");
         btcHind(8205.50);        
     }
@@ -36,7 +36,7 @@ public class Homework1 {
 
 run:
 Arvutame BTC hinnamuutuse alates 01.01.2018.
-Bitcoini hind on aasta algusest langenud 39.91814721639176% ehk 5451.701999999999 eurot.
+Bitcoini hind on aasta algusest langenud 39.91813841783089% ehk 5451.700000000001 eurot.
 BUILD SUCCESSFUL (total time: 0 seconds)
 
 */
