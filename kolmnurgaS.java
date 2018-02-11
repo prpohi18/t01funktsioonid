485af076e34ab59ef0ba2860780505c6dd6311f6
Kalmer Roopa <kalmerr@tlu.ee>
2018-02-11 23:14:16 +0200

    Kodutöö nr.1

---
+++ kolmnurgaS.java
@@ -0,0 +1,12 @@
+
+
+public class kolmnurgaS {
+	public double pindala(double a, double h, double s){
+	
+		s = 0.5 * (a * h);
+		System.out.println("Kolmnurga pindala on: " + s);
+		return s;
+	}
+	
+	
+}
