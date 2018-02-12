087b5c261731d498c8e83758426dbd49f8a7ed0d
Silver Kaugemaa <silverkaugemaa@users.noreply.github.com>
2018-02-12 19:31:54 +0200

    kodutöö lisatud

---
+++ tankimine.java
@@ -0,0 +1,23 @@
+public class tankimine{
+    public static String paagiSeis(int kütus){
+        if(kütus<30){return "kütust on alla poole paagi";}
+        return "ei pea tankima";
+    }
+    
+  
+    public static void main(String[] arg){
+        int kütus=20;
+        try{
+          if(arg.length>0){kütus=Integer.parseInt(arg[0]);}
+		  
+		} catch(Exception e){
+            System.out.println(
+             "töötab ainult kujul: java tankimine [kütus]");
+            System.out.println("java tankimine 40"); 
+            return;        
+        }
+        System.out.println(paagiSeis(kütus));
+    }
+	
+	
+}
